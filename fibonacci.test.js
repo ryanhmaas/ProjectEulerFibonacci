cac38b9c0f400fb2215afa4f24ed17503c29337c
Ryan Maas <ryan.h.maas@gmail.com>
2020-11-13 12:25:16 -0500

    add back tests

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -1,6 +1,5 @@
 import { fibonacciSumEven, fibonacciSortaSnazzy } from './fibonacci';
 
-/*
 // tests to validate implementation
 test('fibonacciSumEven with an input of 10 should return 10 ', () => {
   expect(fibonacciSumEven(10)).toBe(10);
@@ -21,8 +20,6 @@ test('fibonacciSumEven with an input of 4000000 should return 4613732 ', () => {
   expect(fibonacciSumEven(4000000)).toBe(4613732);
 });
 
-*/
-
 // expect these tests to return same results
 test('fibonacciSortaSnazzy with an input of 10 should return 10 ', () => {
   expect(fibonacciSortaSnazzy(10)).toBe(10);
@@ -32,7 +29,6 @@ test('fibonacciSortaSnazzy with an input of 34 should return 44 ', () => {
   expect(fibonacciSortaSnazzy(34)).toBe(44);
 });
 
-/*
 test('fibonacciSortaSnazzy with an input of 34 should return 60696 ', () => {
   expect(fibonacciSortaSnazzy(100000)).toBe(60696);
 });
@@ -40,4 +36,3 @@ test('fibonacciSortaSnazzy with an input of 34 should return 60696 ', () => {
 test('fibonacciSortaSnazzy with an input of 4000000 should return 4613732 ', () => {
   expect(fibonacciSortaSnazzy(4000000)).toBe(4613732);
 });
-*/
